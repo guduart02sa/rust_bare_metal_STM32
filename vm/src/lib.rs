0d8339c07a0423f75369fa9aec2f3d3e4e351392
Gustavo Duarte <63075914+guduart02sa@users.noreply.github.com>
2024-09-16 22:38:15 +0200

    Delete vm directory
    vm incomplete

--- vm/src/lib.rs
+++
@@ -1,5 +0,0 @@
-#![cfg_attr(not(feature = "std"), no_std)]
-
-mod machine;
-
-pub use machine::*;
--- vm/src/main.rs
+++
@@ -1,6 +0,0 @@
-fn main() -> Result<(), interpreter::Error> {
-    let filename = std::env::args().nth(1).unwrap();
-    let buffer = std::fs::read(filename).unwrap();
-    let mut machine = interpreter::Machine::new(&buffer).unwrap();
-    machine.run()
-}
